29498db241845d4531fcf1035e35740f0455107b
钟科 <edzhong3020@sina.com>
2019-03-28 13:53:31 +0800

    修改逻辑
    之前的逻辑太谢特，修改了一下。

--- work/demo.java
+++ work/demo.java
@@ -13,35 +13,39 @@ public class demo {
 	 * agrs:IPV6 addresses String with :: format
 	 * return:IPV6 addresses String in the correct format
 	 * */
-	public static String ipV6AdderssFix(String ipV6){
+	public static String ipV6AdderssFix(String IPV6){
 		StringBuffer sb = new StringBuffer();
-		if(ipV6.contains("::")){
-			if(ipV6.equals("::")){
+		if(IPV6.contains("::")){
+			if(IPV6.equals("::")){
 				return "0000:0000:0000:0000:0000:0000:0000:0000";
 			}
-			String ipV6$ = ipV6.replace("::",":$:");
-			String[] ipV6s = ipV6$.split(":");
-			if(ipV6s[0].isEmpty() && ipV6s[1].equals("$")){
-				for(int i=0;i<10-ipV6s.length;i++){
+			String[] ipV6s = IPV6.split("::");
+			if(ipV6s[0].isEmpty() && ipV6s.length ==2){
+				if(isIPV4Format(IPV6.split("::")[1])){
+					String ipV4 = IPV4ToIPV6(IPV6.split("::")[1]);
+					return ipV6AdderssFix("::".concat(ipV4));
+				}
+				for(int i=0;i<8-ipV6s[1].split(":").length;i++){
 					sb = sb.append("0000:");
 				}
-				return sb.append(ipV6.split("::")[1].toLowerCase()).toString();
-			}else if(ipV6s.length==2 && ipV6s[1].equals("$")){
-				sb = sb.append(ipV6.replace("::",":").toLowerCase());
-				for(int i=0;i<9-ipV6s.length;i++){
-					sb = sb.append("0000:");
+				sb.append(IPV6.split("::")[1].toLowerCase());
+			}else if(ipV6s.length==1){
+				sb = sb.append(ipV6s[0].toLowerCase());
+				for(int i=0;i<8-ipV6s[0].split(":").length;i++){
+					sb.append(":0000");
 				}
-				return sb.substring(0,sb.length()-1);
-			}else if(ipV6s.length==3 && ipV6s[1].equals("$")){
-				sb = sb.append(ipV6.split("::")[0].toLowerCase()).append(":");
-				for(int i=0;i<9-ipV6s.length;i++){
-					sb = sb.append("0000:");
+			}else if(!ipV6s[0].isEmpty() && ipV6s.length==2){
+				sb = sb.append(ipV6s[0].toLowerCase());
+				for(int i=0;i<8-IPV6.replace("::",":").split(":").length;i++){
+					sb = sb.append(":0000");
 				}
-				sb = sb.append(ipV6.split("::")[1].toLowerCase());
-				return sb.toString();
+				sb = sb.append(":").append(ipV6s[1].toLowerCase());
+			}else{
+				return null;
 			}
-        }
-		sb.append(ipV6);
+		}else{
+			sb.append(IPV6);
+		}
 		return completionIPV6(sb.toString());
 	}
 
@@ -64,6 +68,88 @@ public class demo {
 			}
 		}
 		return sb.substring(0,sb.length()-1).toLowerCase();
+	}
+
+	/**
+	 * args:ipV6 addresses String
+	 * return:ture if ipV6 addresses String is correct or false if ipV6 addresses String is wrong
+	 * */
+	public static boolean isIPV6Format(String IPV6){
+		String regex = "^[A-Fa-f0-9]+$";
+		String[] ipV6s = null;
+		if(IPV6.contains("::")){
+			if(IPV6.indexOf("::")==0){
+				if(IPV6.equals("::")){
+					return true;
+				}
+				if(isIPV4Format(IPV6.split("::")[1])){
+					return true;
+				}
+				ipV6s = IPV6.split("::")[1].split(":");
+			}else if(IPV6.indexOf("::")==IPV6.length()-1){
+				ipV6s = IPV6.split("::")[0].split(":");
+			}else{
+				ipV6s = IPV6.replace("::",":").split(":");
+			}
+		}else{
+			ipV6s = IPV6.split(":");
+		}
+		for(String ip:ipV6s){
+			if(ipV6s.length>8 || ip.isEmpty() || ip.length()>4 || !ip.matches(regex)){
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * args:ipV4 addresses String
+	 * return:ture if ipV4 addresses String is correct or false if ipV4 addresses String is wrong
+	 * */
+	public static boolean isIPV4Format(String ip){
+		String[] ips = ip.split("\\.");
+		if(ips.length != 4){
+			return false;
+		}
+		try{
+			for(String ipSplit:ips){
+				Integer.valueOf(ipSplit);
+			}
+		}catch(NumberFormatException e){
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * args:part of ipv4 address which split by point
+	 * return:A string parameter converted to ipv6 format
+	 * */
+	public static String intToHex(int n){
+		StringBuilder sb = new StringBuilder(8);
+		char []b = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+		while(n != 0){
+			sb = sb.append(b[n%16]);
+			n = n/16;
+		}
+		return sb.reverse().toString();
+	}
+
+	/**
+	 * args:The part of an ipv6 string with the ipv4 format
+	 * return:The ipv4 format string is converted to an ipv6 format string
+	 * */
+	public static String IPV4ToIPV6(String ip){
+		StringBuffer sbIPV4 = new StringBuffer();
+		String[] ipV4s = ip.split("\\.");
+		for(int i=0;i<ipV4s.length;i++){
+			String intToHex = intToHex(Integer.valueOf(ipV4s[i]));
+			sbIPV4.append(intToHex);
+			if(i==1){
+				sbIPV4.append(":");
+			}
+		}
+		return sbIPV4.toString();
 	}
 
 	public static void main(String[] args) {
